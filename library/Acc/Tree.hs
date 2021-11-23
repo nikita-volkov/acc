9b423ff71d20847396e0e21d938fc7f3c7a771cf
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-11-23 15:40:27 +0300

    Redesign tree

--- library/Acc/Tree.hs
+++ library/Acc/Tree.hs
@@ -4,27 +4,12 @@ import Acc.Prelude
 import qualified Acc.Prelude as Prelude
 
 data Tree a
-  = -- | Branch with the smaller tree on the left.
-    LeftBranch
-      !Int
-      !(Tree a)
-      !(Tree a)
-  | -- | Branch with the smaller tree on the right.
-    RightBranch
-      !Int
-      !(Tree a)
-      !(Tree a)
-  | Leaf a
+  = BranchTree !(Tree a) a !(Tree a)
+  | LeafTree
 
-prepend :: Tree a -> Tree a -> Tree a
-prepend = \case
-  Leaf lleaf -> \case
-    Leaf rleaf ->
-      LeftBranch 2 (Leaf lleaf) (Leaf rleaf)
-    LeftBranch size rlt rrt ->
-      LeftBranch (succ size) (Leaf lleaf) (LeftBranch size rlt rrt)
-    RightBranch size rlt rrt ->
-      LeftBranch (succ size) (Leaf lleaf) (RightBranch size rlt rrt)
-  LeftBranch lsize llt lrt -> \case
-    LeftBranch rsize rlt rrt ->
-      error "TODO"
+fromList :: [a] -> Tree a
+fromList = reduce LeafTree . reverse
+  where
+    reduce tree = \case
+      a : b -> reduce (BranchTree LeafTree a tree) b
+      [] -> tree
